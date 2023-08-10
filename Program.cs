9d7aa5891983efd990d5827f98d247b693573657
emberogl <embernebulan@gmail.com>
2023-08-10 10:05:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Opgaver
+{
+    class Opgave1
+    {
+        static void Main(string[] args)
+        {
+            int length = 16;
+            double width = 5.1;
+
+            /// Der bliver udskrevet 81.6
+            Console.WriteLine(length * width);
+
+            /// Der kan ikke implicit konverteres typen double til int, så der opstår fejl
+            int area = width * length;
+
+            /// Der opstår ikke fejl, da operationen width * length giver en decimal værdi
+            double d = width * length;
+        }
+    }
+}
